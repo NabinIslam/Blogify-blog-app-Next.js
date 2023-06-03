3ecd345f22b258fee22386dfeec1663db9021011
Nabin Islam <sinabin62246@gmail.com>
2023-06-03 21:11:16 +0600

    firebase config keys transfered to env file

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp} from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyBqHTL7aDWUw85aiitT0tSz8GtA9Nvp6N4',
-    authDomain: 'blogify-6d4ee.firebaseapp.com',
-    projectId: 'blogify-6d4ee',
-    storageBucket: 'blogify-6d4ee.appspot.com',
-    messagingSenderId: '199104290716',
-    appId: '1:199104290716:web:2bb51fceb5edab6a2a3259',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
