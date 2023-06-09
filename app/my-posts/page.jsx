c1c4213aaf6d3d666b550d9e4ab7a8f3ec0e6d3e
Nabin Islam <sinabin62246@gmail.com>
2023-06-09 12:47:09 +0600

    push

--- app/my-posts/page.jsx
+++ app/my-posts/page.jsx
@@ -1,91 +1,91 @@
-// 'use client';
+'use client';
 
-// import ConfirmDelete from '@/components/ConfirmDelete';
-// import { AuthContext } from '@/context/AuthContext';
-// import { Table } from 'flowbite-react';
-// import Link from 'next/link';
-// import { useContext, useState } from 'react';
+import ConfirmDelete from '@/components/ConfirmDelete';
+import { AuthContext } from '@/context/AuthContext';
+import { Table } from 'flowbite-react';
+import Link from 'next/link';
+import { useContext, useState } from 'react';
 
-// const getData = async email => {
-//   const res = await fetch(`/api/v1/blogs/email/${email}`);
+const getData = async email => {
+  const res = await fetch(`/api/v1/blogs/email/${email}`);
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
 
-//   return res.json();
-// };
+  return res.json();
+};
 
-// const MyPosts = async () => {
-//   const [showModal, setShowModal] = useState(false);
-//   const [deletingPostId, setDeletingPostId] = useState(null);
-//   const { user } = useContext(AuthContext);
+const MyPosts = async () => {
+  const [showModal, setShowModal] = useState(false);
+  const [deletingPostId, setDeletingPostId] = useState(null);
+  const { user } = useContext(AuthContext);
 
-//   const usersBlogs = await getData(user?.email);
+  const usersBlogs = await getData(user?.email);
 
-//   return (
-//     <main className="py-20">
-//       <div className="container mx-auto">
-//         <h1 className="text-center font-bold text-4xl mb-20">Your Posts</h1>
-//         {usersBlogs.length === 0 ? (
-//           <h1 className="text-center">
-//             You have no post.{' '}
-//             <Link className="underline" href="/post-blog">
-//               Click here
-//             </Link>{' '}
-//             to post a blog .
-//           </h1>
-//         ) : (
-//           <Table>
-//             <Table.Head>
-//               <Table.HeadCell>Blog title</Table.HeadCell>
-//               <Table.HeadCell>action</Table.HeadCell>
-//               <Table.HeadCell>action</Table.HeadCell>
-//             </Table.Head>
-//             <Table.Body className="divide-y">
-//               {usersBlogs.map(({ title, _id }) => (
-//                 <Table.Row
-//                   key={_id}
-//                   className="bg-white dark:border-gray-700 dark:bg-gray-800"
-//                 >
-//                   <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-//                     <Link className="hover:underline" href={`/blog/${_id}`}>
-//                       {title}
-//                     </Link>
-//                   </Table.Cell>
-//                   <Table.Cell>
-//                     <Link
-//                       href={`/edit-post/${_id}`}
-//                       className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 cursor-pointer"
-//                     >
-//                       <p>Edit</p>
-//                     </Link>
-//                   </Table.Cell>
-//                   <Table.Cell>
-//                     <a
-//                       onClick={() => {
-//                         setDeletingPostId(_id);
-//                         setShowModal(true);
-//                       }}
-//                       className="font-medium text-red-600 hover:underline dark:text-cyan-500 cursor-pointer"
-//                     >
-//                       <p>Delete</p>
-//                     </a>
-//                   </Table.Cell>
-//                 </Table.Row>
-//               ))}
-//             </Table.Body>
-//           </Table>
-//         )}
-//       </div>
+  return (
+    <main className="py-20">
+      <div className="container mx-auto">
+        <h1 className="text-center font-bold text-4xl mb-20">Your Posts</h1>
+        {usersBlogs.length === 0 ? (
+          <h1 className="text-center">
+            You have no post.{' '}
+            <Link className="underline" href="/post-blog">
+              Click here
+            </Link>{' '}
+            to post a blog .
+          </h1>
+        ) : (
+          <Table>
+            <Table.Head>
+              <Table.HeadCell>Blog title</Table.HeadCell>
+              <Table.HeadCell>action</Table.HeadCell>
+              <Table.HeadCell>action</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {usersBlogs.map(({ title, _id }) => (
+                <Table.Row
+                  key={_id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    <Link className="hover:underline" href={`/blog/${_id}`}>
+                      {title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      href={`/edit-post/${_id}`}
+                      className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 cursor-pointer"
+                    >
+                      <p>Edit</p>
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <a
+                      onClick={() => {
+                        setDeletingPostId(_id);
+                        setShowModal(true);
+                      }}
+                      className="font-medium text-red-600 hover:underline dark:text-cyan-500 cursor-pointer"
+                    >
+                      <p>Delete</p>
+                    </a>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        )}
+      </div>
 
-//       <ConfirmDelete
-//         showModal={showModal}
-//         setShowModal={setShowModal}
-//         deletingPostId={deletingPostId}
-//       />
-//     </main>
-//   );
-// };
+      <ConfirmDelete
+        showModal={showModal}
+        setShowModal={setShowModal}
+        deletingPostId={deletingPostId}
+      />
+    </main>
+  );
+};
 
-// export default MyPosts;
+export default MyPosts;
