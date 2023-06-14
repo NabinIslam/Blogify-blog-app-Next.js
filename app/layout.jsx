6ab47c7a44f732a494b0ed0429f1493a58fd5631
Nabin Islam <sinabin62246@gmail.com>
2023-06-14 21:11:17 +0600

    push

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import { Poppins } from 'next/font/google';
 import AuthProvider from '@/context/AuthContext';
 import { Toaster } from 'react-hot-toast';
 import FooterComponent from '@/components/FooterComponent';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -18,40 +18,40 @@ export const metadata = {
   description: 'Worlds no. 1 blog site',
 };
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className} suppressHydrationWarning={true}>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <Header />
-            {children}
-            <FooterComponent />
-          </AuthProvider>
-          <Toaster
-            position="bottom-right"
-            toastOptions={{
-              // Define default options
-              className: '',
-              duration: 5000,
-              style: {
-                background: '#363636',
-                color: '#fff',
-              },
+        {/* <QueryClientProvider client={queryClient}> */}
+        <AuthProvider>
+          <Header />
+          {children}
+          <FooterComponent />
+        </AuthProvider>
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            // Define default options
+            className: '',
+            duration: 5000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
 
-              // Default options for specific types
-              success: {
-                duration: 3000,
-                theme: {
-                  primary: 'green',
-                  secondary: 'black',
-                },
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              theme: {
+                primary: 'green',
+                secondary: 'black',
               },
-            }}
-          />
-        </QueryClientProvider>
+            },
+          }}
+        />
+        {/* </QueryClientProvider> */}
       </body>
     </html>
   );
